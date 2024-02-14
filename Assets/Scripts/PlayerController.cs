1112c679e83779836e36a1133f642e23526d141a
The-Doter <kleonn7632@gmail.com>
2024-02-14 18:07:43 +0800

    Shooter

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravity = 9.8f;
+
+    private float _fallVelocity = 0f;
+
+    private CharacterController _characterController;
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+
+    void FixedUpdate()
+    {
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+    }
+}
