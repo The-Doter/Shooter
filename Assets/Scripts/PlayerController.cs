b17b460e8cd33df06dfbbcce75a2043cb4deaf8b
The-Doter <kleonn7632@gmail.com>
2024-02-15 16:21:24 +0800

    Shooter

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float gravity = 9.8f;
+    public float jumpForce;
+    public float speed;
 
     private float _fallVelocity = 0f;
+    private Vector3 _moveVector;
 
     private CharacterController _characterController;
 
@@ -15,10 +18,50 @@ public class PlayerController : MonoBehaviour
         _characterController = GetComponent<CharacterController>();
     }
 
+    void Update()
+    {
+
+        _moveVector = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _fallVelocity = -jumpForce;
+        }
+
+    }
 
     void FixedUpdate()
     {
+
+        _characterController.Move(_moveVector * Time.fixedDeltaTime * speed);
+
         _fallVelocity += gravity * Time.fixedDeltaTime;
         _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        if(_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
+
     }
 }
