688b44de84e6076d4e892108d4bedc48e197ea11
The-Doter <kleonn7632@gmail.com>
2024-03-02 11:36:36 +0800

    Shooter

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    public float damage = 10f;
+
+    void Start()
+    {
+        Invoke("DestroyFireball", lifeTime);
+    }
+
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+
+
+    void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * speed * Time.fixedDeltaTime;
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);
+        DestroyFireball();
+    }
+
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+    private void DamageEnemy(Collision collision)
+    {
+        var EnemyHealth =  collision.gameObject.GetComponent<EnemyHealth>();
+        if(EnemyHealth != null)
+        {
+            EnemyHealth.DealDamage(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/FireballCaster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballCaster : MonoBehaviour
+{
+
+    public Transform FireballSourceTransform;
+    public Fireball fireballPrefab;
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, FireballSourceTransform.position, FireballSourceTransform.rotation);
+        }
+    }
+}
